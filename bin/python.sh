51cc6eae5d8120b5bdf3a89cb067c63c5ce75016
Koji Kawamura <ijokarumawak@gmail.com>
2022-07-10 09:53:57 +0900

    Adding python app.

---
+++ bin/python.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+CONTAINER_NAME=es-hands-on-python
+. $(dirname $0)/../env.sh
+
+(cd python && docker build -t ijokarumawak/${CONTAINER_NAME} .)
+
+docker ps |grep ${CONTAINER_NAME} > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Docker container ${CONTAINER_NAME} already exists. Removing it first..."
+  docker rm -f ${CONTAINER_NAME}
+fi
+
+echo "Creating a docker container ${CONTAINER_NAME}..."
+docker run --rm --name ${CONTAINER_NAME} --network es-hands-on -d \
+--label co.elastic.logs/processors.1.add_fields.target="event" \
+--label co.elastic.logs/processors.1.add_fields.fields.dataset='${data.container.name}' \
+-e ELASTIC_CLOUD_ID=${ELASTIC_CLOUD_ID} \
+-e ELASTIC_USERNAME=${ELASTIC_USERNAME} \
+-e ELASTIC_PASSWORD=${ELASTIC_PASSWORD} \
+ijokarumawak/${CONTAINER_NAME} \
